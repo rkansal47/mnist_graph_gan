19ad79999ed38831eed08cfe74f6badac389fc88
Raghav Kansal <raghavk1997@yahoo.co.in>
2020-06-19 10:05:00 -0700

    testing

--- mnist_superpixels/torch_geometric_test.py
+++ mnist_superpixels/torch_geometric_test.py
@@ -3,15 +3,36 @@ import torch_geometric
 
 from torch_geometric.datasets import MNISTSuperpixels
 from torch_geometric.data import Data
+from torch_geometric.data import DataLoader
 
 import torch_geometric.transforms as T
 
-dataset = torch.load('dataset/training.pt')
+dataset = torch.load('../mnist_superpixels/raw/training.pt')
 data = Data(dataset)
 
 datast = MNISTSuperpixels(".", True)
 
-dataset5 = MNISTSuperpixels(".", True, transform=T.Polar())
+datast
+
+dataset5 = MNISTSuperpixels(".", True, transform=T.Cartesian())
+
+train_loader = DataLoader(dataset5, batch_size=2)
+
+for dat in train_loader:
+    print(dat)
+    #print(dat.x)
+    print(dat.edge_attr)
+    print(len(dat.edge_attr))
+    print(len(dat.edge_index))
+    break
+
+dat.edge_attr
+
+dat.edge_index.shape
+
+dat.edge_attr.shape
+
+dat.x.shape
 
 dataset[0].shape
 
@@ -36,7 +57,7 @@ torch.unique(data.edge_index[1])
 data.edge_index[0,:75]
 data.pos[0]
 
-dataset2 = torch.load('processed/training.pt')
+dataset2 = torch.load('../mnist_superpixels/processed/training.pt')
 
 
 
