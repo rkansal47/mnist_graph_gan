4dafb74683d84d72924403fe0c53585cb625e430
raghsthebest <raghavk1997@yahoo.co.in>
2020-08-13 14:19:44 +0800

    use training and testing data both

--- mnist_superpixels/superpixels_dataset.py
+++ mnist_superpixels/superpixels_dataset.py
@@ -5,20 +5,31 @@ from torch.utils.data import Dataset
 class SuperpixelsDataset(Dataset):
     def __init__(self, dataset_path, num_thresholded, train=True, intensities=False, num=-1, mnist8m=False):
         if(train):
-            dataset = torch.load(dataset_path + 'training.pt')
+            dataset1 = torch.load(dataset_path + 'training.pt')
+            dataset2 = torch.load(dataset_path + 'test.pt')
+
+            ints1 = dataset1[0]
+            ints2 = dataset2[0]
+            coords1 = dataset1[3]
+            coords2 = dataset2[3]
+
+            if(num > -1):
+                ints1 = ints1[dataset1[4] == num]
+                ints2 = ints2[dataset2[4] == num]
+                coords1 = coords1[dataset1[4] == num]
+                coords2 = coords2[dataset2[4] == num]
+
+            ints = torch.cat((ints1, ints2), axis=0)
+            coords = torch.cat((coords1, coords2), axis=0)
         else:
             dataset = torch.load(dataset_path + 'test.pt')
 
-        print("Dataset Loaded. Shape: ")
+            ints = dataset[0]
+            coords = dataset[3]
 
-        ints = dataset[0]
-        coords = dataset[3]
-
-        # self.y = torch.tensor(dataset[4], dtype=torch.long)
-
-        if(num > -1):
-            ints = ints[dataset[4] == num]
-            coords = coords[dataset[4] == num]
+            if(num > -1):
+                ints = ints[dataset[4] == num]
+                coords = coords[dataset[4] == num]
 
         ints = ints - 0.5
         coords = (coords - 14) / 28
@@ -27,9 +38,8 @@ class SuperpixelsDataset(Dataset):
 
         self.X = X
 
-        # print(X.size())
+        print("Dataset Loaded. Shape: ")
         print(X.shape)
-        # print(X[0])
 
     def __len__(self):
         return len(self.X)
