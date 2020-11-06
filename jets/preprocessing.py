fb0069d20d74e1feec4af55e75b8117a01a8446f
raghsthebest <raghavk1997@yahoo.co.in>
2020-11-06 14:27:29 -0800

    preprocessing t jets

--- jets/preprocessing.py
+++ jets/preprocessing.py
@@ -8,14 +8,15 @@ import torch
 import numpy as np
 
 dir_path = '/graphganvol/datasets/jets/30p/train/'
-dir_path = '/Users/raghav/Documents/Work/CERN/datasets/'
+# dir_path = '/Users/raghav/Documents/Work/CERN/datasets/'
 
 rootfiles = [dir_path + f for f in listdir(dir_path) if isfile(join(dir_path, f))]
 print(rootfiles)
 print(len(rootfiles))
 
-jet_type = 'g'
-particle_features = ['etarel', 'phirel', 'pt']
+pfbool = True  # particle features or jet features
+jet_type = 't'
+particle_features = ['etarel', 'phirel', 'ptrel']
 jet_features = ['pt', 'eta', 'mass']
 
 n = 0
@@ -28,30 +29,28 @@ for f in rootfiles:
     file = h5py.File(f, 'r')
     print(file.keys())
 
-    # pfid = [list(file['particleFeatureNames']).index(b'j1_' + pf.encode('UTF-8')) for pf in particle_features]
+    n_all_jets = file['jets'].shape[0]
     jtid = list(file['jetFeatureNames']).index(b'j_' + jet_type.encode('UTF-8'))
-    jetfsid = [list(file['jetFeatureNames']).index(b'j_' + jf.encode('UTF-8')) for jf in jet_features]
 
-    # n_jets = 0
-    # n_all_jets = file['jets'].shape[0]
+    if pfbool:
+        pfid = [list(file['particleFeatureNames']).index(b'j1_' + pf.encode('UTF-8')) for pf in particle_features]
+        for i in tqdm(range(n_all_jets)):
+            if file['jets'][i][jtid]:
+                tjet = []
+                for particle in file['jetConstituentList'][i]:
+                    pfs = [particle[id] for id in pfid]
+                    tjet.append(pfs)
+                jets.append(tjet)
 
-    gjets = file['jets'][:, jtid]
-    if n == 0:
-        jets = file['jets'][:, jetfsid][gjets == 1]
     else:
-        jets = np.concatenate((jets, file['jets'][:, jetfsid][gjets == 1]))
-
-    # for i in tqdm(range(n_all_jets)):
-    #     if file['jets'][i][jtid]:
-    #         tjet = []
-    #         for particle in file['jetConstituentList'][i]:
-    #             pfs = [particle[id] for id in pfid]
-    #             tjet.append(pfs)
-    #         jets.append(tjet)
-    #         n_jets += 1
+        jetfsid = [list(file['jetFeatureNames']).index(b'j_' + jf.encode('UTF-8')) for jf in jet_features]
+        gjets = file['jets'][:, jtid]
+        if n == 0:
+            jets = file['jets'][:, jetfsid][gjets == 1]
+        else:
+            jets = np.concatenate((jets, file['jets'][:, jetfsid][gjets == 1]))
 
     n += 1
     file.close()
 
-
-torch.save(torch.tensor(jets), './datasets/all_g_jets_30p_jetptetamass.pt')
+torch.save(torch.tensor(jets), './datasets/all_' + jet_type + '_jets_30p_polarrel.pt')
