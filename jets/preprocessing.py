0a7faf0d2782949e35081f2f4d75082f9b5c2a51
Raghav Kansal <raghavk1997@yahoo.co.in>
2020-11-06 22:31:08 +0000

    args

--- jets/preprocessing.py
+++ jets/preprocessing.py
@@ -8,32 +8,31 @@ import torch
 import numpy as np
 
 dir_path = '/graphganvol/datasets/jets/30p/train/'
-dir_path = '/Users/raghav/Documents/Work/CERN/datasets/'
+# dir_path = '/Users/raghav/Documents/Work/CERN/datasets/'
 
 rootfiles = [dir_path + f for f in listdir(dir_path) if isfile(join(dir_path, f))]
 print(rootfiles)
 print(len(rootfiles))
 
 jet_type = 'g'
-particle_features = ['etarel', 'phirel', 'pt']
+particle_features = ['etarel', 'phirel', 'ptrel']
 jet_features = ['pt', 'eta', 'mass']
 
 n = 0
-tot_jets = 0
-jets = []
+jetps = []
 for f in rootfiles:
     print(n)
     print(f)
 
     file = h5py.File(f, 'r')
-    print(file.keys())
+    # print(file.keys())
 
-    # pfid = [list(file['particleFeatureNames']).index(b'j1_' + pf.encode('UTF-8')) for pf in particle_features]
+    pfid = [list(file['particleFeatureNames']).index(b'j1_' + pf.encode('UTF-8')) for pf in particle_features]
     jtid = list(file['jetFeatureNames']).index(b'j_' + jet_type.encode('UTF-8'))
     jetfsid = [list(file['jetFeatureNames']).index(b'j_' + jf.encode('UTF-8')) for jf in jet_features]
 
     # n_jets = 0
-    # n_all_jets = file['jets'].shape[0]
+    n_all_jets = file['jets'].shape[0]
 
     gjets = file['jets'][:, jtid]
     if n == 0:
@@ -41,17 +40,17 @@ for f in rootfiles:
     else:
         jets = np.concatenate((jets, file['jets'][:, jetfsid][gjets == 1]))
 
-    # for i in tqdm(range(n_all_jets)):
-    #     if file['jets'][i][jtid]:
-    #         tjet = []
-    #         for particle in file['jetConstituentList'][i]:
-    #             pfs = [particle[id] for id in pfid]
-    #             tjet.append(pfs)
-    #         jets.append(tjet)
-    #         n_jets += 1
+    for i in tqdm(range(n_all_jets)):
+        if file['jets'][i][jtid]:
+            tjet = []
+            for particle in file['jetConstituentList'][i]:
+                pfs = [particle[id] for id in pfid]
+                tjet.append(pfs)
+            jetps.append(tjet)
 
     n += 1
     file.close()
 
 
 torch.save(torch.tensor(jets), './datasets/all_g_jets_30p_jetptetamass.pt')
+torch.save(torch.tensor(jetps), './datasets/all_g_jets_30p_polarrel.pt')
