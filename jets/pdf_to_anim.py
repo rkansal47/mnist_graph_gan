12f45fc21c7903e463e681d0382bccbcd09667c0
rkansal47 <raghavk1997@yahoo.co.in>
2021-03-03 10:04:59 -0800

    pdf to anim update

--- jets/pdf_to_anim.py
+++ jets/pdf_to_anim.py
@@ -7,13 +7,13 @@ from tqdm import tqdm
 figdir = '/graphganvol/mnist_graph_gan/jets/figs/150_g30_anim/'
 
 images = []
-for i in tqdm(range(151)):
+for i in tqdm(range(150)):
     fullfname = figdir + str(i) + 'p.pdf'
     images.append(pdf2image.convert_from_path(fullfname))
 
-videodims = (5751, 1339)
+videodims = (4147, 1340)
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-video = cv2.VideoWriter(figdir + "anim.mp4", fourcc, 60, videodims)
+video = cv2.VideoWriter(figdir + "anim.mp4", fourcc, 10, videodims)
 
 for i in tqdm(range(len(images))):
     video.write(np.array(images[i][0]))
