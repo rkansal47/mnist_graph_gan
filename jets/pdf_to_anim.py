88120a8281c0220f81f2581b32256a6142639938
rkansal47 <raghavk1997@yahoo.co.in>
2021-03-02 21:21:47 -0800

    pdf anim unix fix

--- jets/pdf_to_anim.py
+++ jets/pdf_to_anim.py
@@ -1,27 +1,21 @@
 import pdf2image
-from os import listdir
 import cv2
 import numpy as np
+from tqdm import tqdm
 
 
 figdir = '/graphganvol/mnist_graph_gan/jets/figs/66_g30/'
 
-fnames = listdir(figdir)
-
-print(fnames)
-
-# fnames = fnames[1:]
-
 images = []
-for fname in fnames:
-    fullfname = figdir + fname
+for i in tqdm(range(659)):
+    fullfname = figdir + str(i) + '.pdf'
     images.append(pdf2image.convert_from_path(fullfname))
 
 videodims = (5751, 1339)
-fourcc = cv2.VideoWriter_fourcc(*'avc1')
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 video = cv2.VideoWriter(figdir + "anim.mp4", fourcc, 60, videodims)
 
-for im in images:
-    video.write(np.array(im[0]))
+for i in tqdm(range(659)):
+    video.write(np.array(images[i][0]))
 
 video.release()
