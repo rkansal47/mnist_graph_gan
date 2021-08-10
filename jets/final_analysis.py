e9aa47f1776f152ca5b19f38e8bc6123943aebea
root <root@rk-8-v100s-2.c.harrisgroup-223921.internal>
2021-08-10 17:08:17 +0000

    args + fix

--- jets/final_analysis.py
+++ jets/final_analysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import pandas as pd
+import shutil
 
 dirs = os.listdir('losses')
 
@@ -11,8 +12,9 @@ loss_keys = ['fpnd', 'coverage', 'mmd']
 scores = []
 names = []
 
-for i in range(263, 269):
+for i in range(270, 279):
     name = loss_map[str(i)]
+    print(name)
     names.append(name)
     score_arr = []
     loss_dir = 'losses/' + name + '/'
@@ -22,6 +24,24 @@ for i in range(263, 269):
     w1m = w1j[:, 0]
     w1mstd = w1jstd[:, 0]
     min_epoch = np.argmin(w1m)
+
+    final_models_dir = 'final_models/'
+    if 'treegang_rgand' in name: final_models_dir += 'treeganfc_'
+    if 'treegang_pointnetd' in name: final_models_dir += 'treeganpnet_'
+    if 'pcgang' in name: final_models_dir += 'pcgan_'
+
+    if 'g30' in name: final_models_dir += 'g/'
+    if 't30' in name: final_models_dir += 't/'
+    if 'q30' in name: final_models_dir += 'q/'
+
+    print(final_models_dir)
+
+    if not os.path.exists(final_models_dir): os.makedirs(final_models_dir)
+    shutil.copy(f"args/{name}.txt", final_models_dir)
+    shutil.copy(f"models/{name}/G_{min_epoch * 5}.pt", final_models_dir)
+    shutil.copy(f"models/{name}/D_{min_epoch * 5}.pt", final_models_dir)
+
+
     score_arr.append(min_epoch * 5)
     score_arr.append(w1m[min_epoch])
     score_arr.append(w1mstd[min_epoch])
@@ -44,4 +64,4 @@ for i in range(263, 269):
     scores.append(score_arr)
 
 
-pd.DataFrame(scores, names).to_csv('scores.csv')
+pd.DataFrame(scores, names).to_csv('scores2.csv')
--- jets/gen_final_samples.py
+++ jets/gen_final_samples.py
@@ -3,7 +3,10 @@ import torch
 import utils
 from jets_dataset import JetsDataset
 from model import Graph_GAN
-from ext_models import rGANG, GraphCNNGANG
+from ext_models import rGANG, GraphCNNGANG, TreeGANG
+from pcgan_model import latent_G
+from pcgan_model import G as G_pc
+
 
 import os
 import shutil
@@ -73,7 +76,7 @@ for dir in dirs:
 
     args = eval(open(path + args_file).read())
     args['device'] = device
-    args['batch_size'] = 512
+    args['batch_size'] = 1024
     args['datasets_path'] = 'datasets/'
     args = utils.objectview(args)
 
@@ -81,22 +84,34 @@ for dir in dirs:
     model_name = dir.split('_')[0]
     print(model_name)
 
-    if model_name == 'fcpnet':
-        G = rGANG(args).to(device)
-    elif model_name == 'graphcnnpnet':
-        G = GraphCNNGANG(args).to(device)
-    elif model_name == 'mppnet':
-        G = Graph_GAN(True, args).to(device)
+    if 'treegan' in model_name:
+        continue
+        G = TreeGANG(args.treegang_features, args.treegang_degrees, args.treegang_support).to(device)
+        pcgan_args = None
+    elif model_name == 'pcgan':
+        Gpc = G_pc(args.node_feat_size, args.pcgan_z1_dim, args.pcgan_z2_dim).to(device)
+        Gpc.load_state_dict(torch.load(f"models/pcgan/pcgan_G_pc_{args.jets}.pt", map_location=args.device))
+        G = latent_G(args.pcgan_latent_dim, args.pcgan_z1_dim).to(device)
+        pcgan_args = {'sample_points': True, 'G_pc': Gpc}
     else: continue
 
-    X = JetsDataset(args)
+    # if model_name == 'fcpnet':
+    #     G = rGANG(args).to(device)
+    # elif model_name == 'graphcnnpnet':
+    #     G = GraphCNNGANG(args).to(device)
+    # elif model_name == 'mppnet':
+    #     G = Graph_GAN(True, args).to(device)
+    # else: continue
 
-    labels = X[:][1].to(device)
+    # X = JetsDataset(args)
+
+    # labels = X[:][1].to(device)
+    labels = None
 
     print(G)
     print(path + G_file)
     G.load_state_dict(torch.load(path + G_file, map_location=device))
 
     G.eval()
-    gen_out = utils.gen_multi_batch(args, G, 100000, labels=labels, use_tqdm=True)
+    gen_out = utils.gen_multi_batch(args, G, 100000, labels=labels, use_tqdm=True, pcgan_args=pcgan_args)
     np.save(path + "samples.npy", gen_out)
